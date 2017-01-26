3c8a7711e8f726114c909179faaf0e5b64395e98
MrWooJ <hurricanc@gmail.com>
2017-01-26 18:00:50 +0330

    Add Routes and Additional Required Files to Project

--- config/routes.js
+++ config/routes.js
@@ -2,31 +2,81 @@ exports['default'] = {
   routes: function (api) {
     return {
 
-      /* ---------------------
-      routes.js
-
-      For web clients (http and https) you can define an optional RESTful mapping to help route requests to actions.
-      If the client doesn't specify and action in a param, and the base route isn't a named action, the action will attempt to be discerned from this routes.js file.
-
-      Learn more here: http://www.actionherojs.com/docs/#routes
-
-      examples:
-
-      get: [
-        { path: '/users', action: 'usersList' }, // (GET) /api/users
-        { path: '/search/:term/limit/:limit/offset/:offset', action: 'search' }, // (GET) /api/search/car/limit/10/offset/100
+      get: [{
+          path: '/account/announcer/:accountHashID',
+          action: 'getAccountModelAction'
+        },
+        {
+          path: '/account/:accountHashID/campaign',
+          action: 'getCampaignListAction'
+        },
+        {
+          path: '/account/:accountHashID/campaign/:campaignHashID',
+          action: 'getCampaignModelAction'
+        },
+        {
+          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign',
+          action: 'getSubcampaignListAction'
+        },
+        {
+          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID',
+          action: 'getSubcampaignModelAction'
+        },
+        {
+          path: '/campaign/:campaignHashID/setting',
+          action: 'getCampaignSettingAction'
+        },
+        {
+          path: '/campaign/:campaignHashID/subcampaign/:subCampaignHashID/setting',
+          action: 'getSubcampaignSettingAction'
+        }
       ],
 
-      post: [
-        { path: '/login/:userID(^\\d{3}$)', action: 'login' } // (POST) /api/login/123
+      post: [{
+          path: '/account/announcer/:accountHashID',
+          action: 'setAccountModelAction'
+        },
+        {
+          path: '/account/:accountHashID/campaign',
+          action: 'setCampaignModelAction'
+        },
+        {
+          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign',
+          action: 'setSubcampaignModelAction'
+        }
       ],
 
-      all: [
-        { path: '/user/:userID', action: 'user', matchTrailingPathParts: true } // (*) /api/user/123, api/user/123/stuff
+      put: [{
+          path: '/account/announcer/:accountHashID',
+          action: 'updateAccountModelAction'
+        },
+        {
+          path: '/account/:accountHashID/campaign/:campaignHashID',
+          action: 'updateCampaignModelAction'
+        },
+        {
+          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID',
+          action: 'updateSubcampaignModelAction'
+        },
+        {
+          path: '/campaign/:campaignHashID/setting',
+          action: 'saveCampaignSettingAction'
+        },
+        {
+          path: '/campaign/:campaignHashID/subcampaign/:subCampaignHashID/setting',
+          action: 'saveSubcampaignSettingAction'
+        }
+      ],
+
+      delete: [{
+          path: '/account/:accountHashID/campaign/:campaignHashID',
+          action: 'deleteCampaignModelAction'
+        },
+        {
+          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID',
+          action: 'deleteSubcampaignModelAction'
+        }
       ]
-
-      ---------------------- */
-
     }
   }
 }
