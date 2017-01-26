f719637050600b641659c1790f1d26f4e45a56a9
MrWooJ <hurricanc@gmail.com>
2017-01-26 12:08:24 +0330

    Add Useful Utility Methods to Workstation

---
+++ public/method/utility.js
@@ -0,0 +1,31 @@
+var uuid = require('node-uuid')
+var countryList = require('../config/country.json')
+var configuration = require('../config/configuration.json')
+
+module.exports = {
+  generateQueryString: function (data) {
+    var ret = [];
+    for (var d in data)
+      if (data[d])
+        ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+    return ret.join("&");
+  },
+
+  base64Encoding: function (data) {
+    return new Buffer(data).toString('base64');
+  },
+
+  generateUniqueHashID: function () {
+    return uuid.v1();
+  },
+
+  getUnixTimeStamp: function () {
+    return Math.floor((new Date).getTime() / 1000);
+  },
+
+  mapCountryToTable: function (countryCode) {
+    for (var i = 0; i < countryList.length; i++)
+      if (countryList[i].code === countryCode)
+        return countryCode + ':'
+  }
+}
