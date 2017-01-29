0c750bf56cc25a10c9a2b10e34482dff99c4f583
MrWooJ <hurricanc@gmail.com>
2017-01-29 19:04:48 +0330

    Implement Get AccountModel Business Logic

--- logic/account/readAccountModelLogic.js
+++ logic/account/readAccountModelLogic.js
@@ -1,5 +1,18 @@
+var configuration = require('../../config/configuration.json')
+
 module.exports = {
   getAccountModel: function (redisClient, accountHashID, callback) {
-    
+    var tableName = configuration.TableMAAccountModelAnnouncerAccountModel + accountHashID
+    redisClient.hmget(tableName,
+      configuration.ConstantAMAAMBudget,
+      configuration.ConstantAMAAMAccountType,
+      function (err, replies) {
+        if (err) {
+          callback(err, null)
+          return
+        }
+        callback(null, replies)
+      }
+    )
   }
 }
