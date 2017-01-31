c60eb26f44805524bee4fe894887671f9c7d503e
MrWooJ <hurricanc@gmail.com>
2017-01-31 12:58:41 +0330

    Implement Get Campaign Model Simply by HMGET

--- logic/campaign/readCampaignModelLogic.js
+++ logic/campaign/readCampaignModelLogic.js
@@ -1,5 +1,29 @@
+var configuration = require('../../config/configuration.json')
+var utility = require('../../public/method/utility')
+
 module.exports = {
-  getCampaignModel: function (redisClient, accountHashID, campaignHashID, callback) {
+  getCampaignModel: function (redisClient, campaignHashID, callback) {
+    var tableName = configuration.TableMACampaignModel + campaignHashID
+    redisClient.hmget(tableName,
+      configuration.ConstantCMBudget,
+      configuration.ConstantCMBeginningTime,
+      configuration.ConstantCMEndingTime,
+      configuration.ConstantCMCampaignStatus,
+      configuration.ConstantCMCampaignName,
+      configuration.ConstantCMStartStyle,
+      configuration.ConstantCMSettingStyle,
+      configuration.ConstantCMMediaStyle,
+      configuration.ConstantCMWebhookIdentifier,
+      configuration.ConstantCMMessage,
+      function (err, replies) {
+        if (err) {
+          callback(err, null)
+          return
+        }
+        callback(null, replies)
+      }
+    )
+  },
 
   },
   getCampaignList: function (redisClient, accountHashID, callback) {
