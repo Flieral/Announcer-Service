716ade6bc3c0c7e5551fa12b1f03374e61257b35
MrWooJ <hurricanc@gmail.com>
2017-01-31 12:59:44 +0330

    Implement "List" of CampaignHashId(s) Retrieval (By Filter)

--- logic/campaign/readCampaignModelLogic.js
+++ logic/campaign/readCampaignModelLogic.js
@@ -25,6 +25,53 @@ module.exports = {
     )
   },
 
+  getCampaignList: function (redisClient, accountHashID, filter, callback) {
+    var filterKeys = Object.keys(filter)
+    if (filterKeys.length == 0) {
+      var tableName = configuration.TableMSAccountModelMonitorModel + accountHashID
+      redisClient.zrange(tableName, '0', '-1', 'WITHSCORES', function (err, replies) {
+        if (err) {
+          callback(err, null)
+          return
+        }
+        callback(null, replies)
+      })
+    }
+    else {
+      var destinationTableName = configuration.TableMACampaignModel + configuration.TableTemporary + utility.generateUniqueHashID()
+      var result
+      var args = []
+      args.push(destinationTableName)
+      args.push(filterKeys.length)
+      for (var i = 0; i < filterKeys.length; i++) {
+        var key = configuration.campaignEnum[filterKeys[i]]
+        var table = configuration.TableName.general.CampaignModel + accountHashID
+        utility.stringReplace(table, '@', key)
+        args.push(table)
+      }
+      args.push('AGGREGATE')
+      args.push('MAX')
+      redisClient.zinterstore(args, function (err, replies) {
+        if (err) {
+          callback(err, null)
+          return
+        }
+        redisClient.zrange(destinationTableName, '0', '-1', 'WITHSCORES', function (err, replies) {
+          if (err) {
+            callback(err, null)
+            return
+          }
+          result = replies
+          redisClient.zremrangebyrank(destinationTableName, '0', '-1', function (err, replies) {
+            if (err) {
+              callback(err, null)
+              return
+            }
+            callback(null, result)
+          })
+        })
+      })
+    }
   },
   getCampaignList: function (redisClient, accountHashID, callback) {
 
