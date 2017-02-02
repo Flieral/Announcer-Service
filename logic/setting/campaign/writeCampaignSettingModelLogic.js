b1c0ed1868d8d1aabeb8d76f4fe8d6db10403516
MrWooJ <hurricanc@gmail.com>
2017-02-02 16:58:40 +0330

    Implement Create a Campaign Model Setting Dynamically

--- logic/setting/campaign/writeCampaignSettingModelLogic.js
+++ logic/setting/campaign/writeCampaignSettingModelLogic.js
@@ -2,7 +2,79 @@ var configuration = require('../../../config/configuration.json')
 var utility = require('../../../public/method/utility')
 
 module.exports = {
-  setCampaignSettingModel: function (redisClient, campaignHashID, payload, callback) {
+  setCampaignSettingModel: function (redisClient, accountHashID, campaignHashID, payload, callback) {
+    var table
+    var score = utility.getUnixTimeStamp()
+    var multi = redisClient.multi()
+    var settingKeys = Object.keys(configuration.settingEnum)
 
+    var tableName = configuration.TableMACampaignModelSettingModel + campaignHashID
+    multi.hset(tableName, configuration.ConstantSMPriority, payload[configuration.ConstantSMPriority])
+
+    var finalCounter = 0
+    for (var i = 0; i < settingKeys.length; i++) {
+      if (settingKeys[i] === configuration.settingEnum.Priority)
+        continue
+
+      var key = settingKeys[i]
+      var scoreMemberArray = []
+      var keyValueArray = payload[key]
+
+      for (var j = 0; j < keyValueArray.length; j++) {
+        scoreMemberArray.push(score)
+        scoreMemberArray.push(keyValueArray[j])
+      }
+
+      var counter = 0
+      for (var j = 0; j < keyValueArray.length; j++) {
+        table = configuration.TableModel.general.CampaignModel + accountHashID
+        utility.stringReplace(table, '@', key)
+        
+        redisClient.zrange(table, '0', '-1', function (err, replies) {
+          if (err) {
+            callback(err, null)
+            return
+          }
+
+          table = configuration.TableModel.general.CampaignModel + accountHashID
+          for (var k = 0; k < replies.length; k++) {
+            utility.stringReplace(table, '@', replies[k])
+            multi.zrem(table, campaignHashID)
+          }
+
+          /* Add to Model List */
+          table = configuration.TableModel.general.CampaignModel + accountHashID
+          utility.stringReplace(table, '@', keyValueArray[j])
+          multi.zadd(table, score, campaignHashID)
+          /* Add to Model Set */
+          table = configuration.TableModel.general.CampaignModel
+          utility.stringReplace(table, '@', keyValueArray[j])
+          multi.zadd(table, score, campaignHashID)
+
+          counter++
+        })
+
+        if (counter == keyValueArray.length) {
+          /* Model Set */
+          table = configuration.TableModel.general.CampaignModel + campaignHashID
+          utility.stringReplace(table, '@', key)
+          /* Remove from Model Set */
+          multi.zremrangebyrank(table, '0', '-1')
+          /* Add to Model Set */
+          scoreMemberArray.unshift(table)
+          multi.zadd(scoreMemberArray)
+        }
+      }
+
+      if (finalCounter == settingKeys.length) {
+        multi.exec(function (err, replies) {
+          if (err) {
+            callback(err, null)
+            return
+          }
+          callback(null, configuration.message.setting.campaign.set.successful)
+        })
+      }
+    }
   }
 }
