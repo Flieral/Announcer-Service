7307823b5d3d5bb1bbfdeab410bddff7a3c67430
MrWooJ <hurricanc@gmail.com>
2017-02-03 18:11:39 +0330

    Add Filter Object and Complex Flag to REST Campaign List Action

--- actions/campaign/getCampainListAction.js
+++ actions/campaign/getCampainListAction.js
@@ -3,6 +3,18 @@ var readCampaignModelLogic = require('../../logic/campaign/readCampaignModelLogi
 var Input = {
   accountHashID: {
     required: true
+  },
+  filterObject: {
+    required: false,
+    formatter: function (param, connection, actionTemplate) {
+      return JSON.parse(new Buffer(param, 'base64'))
+    }
+  },
+  complexModel: {
+    required: flase,
+    default: function (param, connection, actionTemplate) {
+      return false;
+    },
   }
 }
 
@@ -12,14 +24,26 @@ exports.getCampaignListAction = {
   inputs: Input,
 
   run: function (api, data, next) {
-    
-    readCampaignModelLogic.getCampaignList(api.redisClient, data.params.accountHashID, function (err, replies) {
-      if (err) {
-        data.response.error = err.error
-        next(err)
-      }
-      data.response.result = replies
-      next()
-    })
+
+    if (data.params.complexModel) {
+      readCampaignModelLogic.getCampaignListComplex(api.redisClient, data.params.accountHashID, data.params.filterObject, function (err, replies) {
+        if (err) {
+          data.response.error = err.error
+          next(err)
+        }
+        data.response.result = replies
+        next()
+      })
+    }
+    else {
+      readCampaignModelLogic.getCampaignList(api.redisClient, data.params.accountHashID, data.params.filterObject, function (err, replies) {
+        if (err) {
+          data.response.error = err.error
+          next(err)
+        }
+        data.response.result = replies
+        next()
+      })
+    }
   }
 }
