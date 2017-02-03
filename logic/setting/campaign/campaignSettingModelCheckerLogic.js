681dec36a590abb139e47ee61900625bf12fa46d
MrWooJ <hurricanc@gmail.com>
2017-02-03 18:34:21 +0330

    Implement Checkers of Campaign and Subcampaign Setting

--- logic/setting/campaign/campaignSettingModelCheckerLogic.js
+++ logic/setting/campaign/campaignSettingModelCheckerLogic.js
@@ -1,11 +1,24 @@
 var configuration = require('../../../config/configuration.json')
+var campaignModelCheckerLogic = require('../../campaign/campaignModelCheckerLogic')
 
 module.exports = {
-  checkCampaignSettingModelForExistence: function (redisClient, campaignHashID, callback) {
-
+  checkCampaignSettingModelForExistence: function (redisClient, accountHashID, campaignHashID, callback) {
+    campaignModelCheckerLogic.checkCampaignModelForExistence(redisClient, accountHashID, campaignHashID, function (err, result) {
+      if (err) {
+        callback(err, null)
+        return
+      }
+      callback(null, callback(null, configuration.message.campaign.setting.exist))
+    })
   },
 
-  checkCampaignSettingModelForNotExistence: function (redisClient, campaignHashID, callback) {
-
+  checkCampaignSettingModelForNotExistence: function (redisClient, accountHashID, campaignHashID, callback) {
+    campaignModelCheckerLogic.checkCampaignModelForNotExistence(redisClient, accountHashID, campaignHashID, function (err, result) {
+      if (err) {
+        callback(err, null)
+        return
+      }
+      callback(null, callback(null, configuration.message.campaign.setting.notExist))
+    })
   }
 }
--- logic/setting/subcampaign/subcampaignSettingModelCheckerLogic.js
+++ logic/setting/subcampaign/subcampaignSettingModelCheckerLogic.js
@@ -1,11 +1,37 @@
 var configuration = require('../../../config/configuration.json')
+var campaignModelCheckerLogic = require('../../campaign/campaignModelCheckerLogic')
+var subcampaignModelCheckerLogic = require('../../subcampaign/subcampaignModelCheckerLogic')
 
 module.exports = {
-  checksubCampaignSettingModelForExistence: function (redisClient, campaignHashID, subCampaignHashID, callback) {
-
+  checksubCampaignSettingModelForExistence: function (redisClient, accountHashID, campaignHashID, subCampaignHashID, callback) {
+    campaignModelCheckerLogic.checkCampaignModelForExistence(redisClient, accountHashID, campaignHashID, function (err, result) {
+      if (err) {
+        callback(err, null)
+        return
+      }
+      subcampaignModelCheckerLogic.checkSubcampaignModelForExistence(redisClient, campaignHashID, subCampaignHashID, function (err, result) {
+        if (err) {
+          callback(err, null)
+          return
+        }
+        callback(null, callback(null, configuration.message.subcampaign.setting.exist))
+      })
+    })
   },
 
-  checksubCampaignSettingModelForNotExistence: function (redisClient, campaignHashID, subCampaignHashID, callback) {
-
+  checksubCampaignSettingModelForNotExistence: function (redisClient, accountHashID, campaignHashID, subCampaignHashID, callback) {
+    campaignModelCheckerLogic.checkCampaignModelForNotExistence(redisClient, accountHashID, campaignHashID, function (err, result) {
+      if (err) {
+        callback(err, null)
+        return
+      }
+      subcampaignModelCheckerLogic.checkSubcampaignModelForNotExistence(redisClient, campaignHashID, subCampaignHashID, function (err, result) {
+        if (err) {
+          callback(err, null)
+          return
+        }
+        callback(null, callback(null, configuration.message.subcampaign.setting.notExist))
+      })
+    })
   }
 }
