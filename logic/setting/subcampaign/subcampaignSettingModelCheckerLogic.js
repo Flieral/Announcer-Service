e854df9ae0f9362cac92a9961b2480cbf555fd10
mohammad4x <m.ferdosi94@gmail.com>
2017-02-04 00:55:32 +0330

    Fix Existence and NotExistence Chain in Logics

--- logic/setting/subcampaign/subcampaignSettingModelCheckerLogic.js
+++ logic/setting/subcampaign/subcampaignSettingModelCheckerLogic.js
@@ -1,37 +1,24 @@
 var configuration = require('../../../config/configuration.json')
-var campaignModelCheckerLogic = require('../../campaign/campaignModelCheckerLogic')
 var subcampaignModelCheckerLogic = require('../../subcampaign/subcampaignModelCheckerLogic')
 
 module.exports = {
   checksubCampaignSettingModelForExistence: function (redisClient, accountHashID, campaignHashID, subCampaignHashID, callback) {
-    campaignModelCheckerLogic.checkCampaignModelForExistence(redisClient, accountHashID, campaignHashID, function (err, result) {
+    subcampaignModelCheckerLogic.checkSubcampaignModelForExistence(redisClient, accountHashID, campaignHashID, subCampaignHashID, function (err, result) {
       if (err) {
         callback(err, null)
         return
       }
-      subcampaignModelCheckerLogic.checkSubcampaignModelForExistence(redisClient, campaignHashID, subCampaignHashID, function (err, result) {
-        if (err) {
-          callback(err, null)
-          return
-        }
-        callback(null, callback(null, configuration.message.subcampaign.setting.exist))
-      })
+      callback(null, callback(null, configuration.message.subcampaign.setting.exist))
     })
   },
 
   checksubCampaignSettingModelForNotExistence: function (redisClient, accountHashID, campaignHashID, subCampaignHashID, callback) {
-    campaignModelCheckerLogic.checkCampaignModelForNotExistence(redisClient, accountHashID, campaignHashID, function (err, result) {
+    subcampaignModelCheckerLogic.checkSubcampaignModelForNotExistence(redisClient, accountHashID, campaignHashID, subCampaignHashID, function (err, result) {
       if (err) {
         callback(err, null)
         return
       }
-      subcampaignModelCheckerLogic.checkSubcampaignModelForNotExistence(redisClient, campaignHashID, subCampaignHashID, function (err, result) {
-        if (err) {
-          callback(err, null)
-          return
-        }
-        callback(null, callback(null, configuration.message.subcampaign.setting.notExist))
-      })
+      callback(null, callback(null, configuration.message.subcampaign.setting.notExist))
     })
   }
 }
