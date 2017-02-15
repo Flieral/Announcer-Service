7eabc8a0606d68ed13b5a1ccf88663cf83cfb65c
mohammad4x <m.ferdosi94@gmail.com>
2017-02-15 18:52:07 +0330

    Modify Base URL

--- config/routes.js
+++ config/routes.js
@@ -3,77 +3,77 @@ exports['default'] = {
     return {
 
       get: [{
-          path: '/account/announcer/:accountHashID',
+          path: '/:apiVersion/account/announcer/:accountHashID',
           action: 'getAccountModelAction'
         },
         {
-          path: '/account/:accountHashID/campaign',
+          path: '/:apiVersion/account/:accountHashID/campaign',
           action: 'getCampaignListAction'
         },
         {
-          path: '/account/:accountHashID/campaign/:campaignHashID',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID',
           action: 'getCampaignModelAction'
         },
         {
-          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID/subcampaign',
           action: 'getSubcampaignListAction'
         },
         {
-          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID',
           action: 'getSubcampaignModelAction'
         },
         {
-          path: '/account/:accountHashID/campaign/:campaignHashID/setting',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID/setting',
           action: 'getCampaignSettingAction'
         },
         {
-          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID/setting',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID/setting',
           action: 'getSubcampaignSettingAction'
         }
       ],
 
       post: [{
-          path: '/account/announcer/:accountHashID',
+          path: '/:apiVersion/account/announcer/:accountHashID',
           action: 'setAccountModelAction'
         },
         {
-          path: '/account/:accountHashID/campaign',
+          path: '/:apiVersion/account/:accountHashID/campaign',
           action: 'setCampaignModelAction'
         },
         {
-          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID/subcampaign',
           action: 'setSubcampaignModelAction'
         }
       ],
 
       put: [{
-          path: '/account/announcer/:accountHashID',
+          path: '/:apiVersion/account/announcer/:accountHashID',
           action: 'updateAccountModelAction'
         },
         {
-          path: '/account/:accountHashID/campaign/:campaignHashID',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID',
           action: 'updateCampaignModelAction'
         },
         {
-          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID',
           action: 'updateSubcampaignModelAction'
         },
         {
-          path: '/account/:accountHashID/campaign/:campaignHashID/setting',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID/setting',
           action: 'saveCampaignSettingAction'
         },
         {
-          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID/setting',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID/setting',
           action: 'saveSubcampaignSettingAction'
         }
       ],
 
       delete: [{
-          path: '/account/:accountHashID/campaign/:campaignHashID',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID',
           action: 'deleteCampaignModelAction'
         },
         {
-          path: '/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID',
+          path: '/:apiVersion/account/:accountHashID/campaign/:campaignHashID/subcampaign/:subCampaignHashID',
           action: 'deleteSubcampaignModelAction'
         }
       ]
